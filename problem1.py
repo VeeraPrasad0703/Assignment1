80a324de6b699602ea6a2523b392dfe656ab58be
VeeraPrasad0703 <130963141+VeeraPrasad0703@users.noreply.github.com>
2023-04-30 14:40:43 +0530

    problem1

--- problem1.py
+++ problem1.py
@@ -1,7 +1,13 @@
 book1=int(input("Enter number of books of type 1:"))
 book2=int(input("Enter number of books of type 2:"))
 book3=int(input("Enter number of books of type 3:"))
-if book1>0 or book2>0 or book3>0: #checking atleat one book is selected or not
+if book1>0 or book2>0 or book3>0:
+    if book1<0:
+        book1=0
+    if book2<0:
+        book2=0
+    if book3<0:
+        book3=0#checking atleat one book is selected or not
     total=book1*499.0+book2*855.0+book3*645.0 #calculating total price
     invoice=total+total*0.12+200 #including gst and delivery charges
     print(invoice)
